345da5d8cf73c2ad1feefa8c52141cb9e96a3f90
nilg5120 <103954917+nilg5120@users.noreply.github.com>
2023-07-19 22:54:51 +0900

    Add files via upload
    HTML追加

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showHello(Model model) {
+        model.addAttribute("title", "Hello World!");
+        model.addAttribute("message", "ようこそ、ばったん技術ブログへ");
+        return "hello";
     }
-
 }
