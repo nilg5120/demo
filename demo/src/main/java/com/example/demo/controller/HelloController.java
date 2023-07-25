281e74fa53afce599964c80cf2d2feecb5c204e2
nilg5120 <103954917+nilg5120@users.noreply.github.com>
2023-07-25 23:39:33 +0900

    登録追加

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -3,30 +3,33 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.example.demo.entity.Todo;
+import com.example.demo.form.TodoData;
+import com.example.demo.repository.TodoRepository;
+import com.example.demo.service.TodoService;
+import lombok.AllArgsConstructor;
 import java.util.Random;
 import jakarta.servlet.http.HttpSession;
-import lombok.AllArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.example.demo.History;
-import com.example.demo.entity.Todo;
-import com.example.demo.repository.TodoRepository;
-
 
 @Controller
 @AllArgsConstructor
 public class HelloController {
 
     private final TodoRepository todoRepository;
+    private final TodoService todoService;
 
     @Autowired
     HttpSession session;
@@ -90,6 +93,38 @@ public class HelloController {
         List<Todo>todoList = todoRepository.findAll();
         mv.addObject("todoList", todoList);
         return mv;
+    }
+
+    @GetMapping("/todo/create")
+    public ModelAndView createTodo(ModelAndView mv) {
+        //登録画面に遷移
+        System.out.println("showCreate");
+        mv.setViewName("todoForm");
+        mv.addObject("todoData",new TodoData());
+        return mv;
+    }
+
+    @PostMapping("/todo/create")
+    public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, ModelAndView mv) {
+        
+        //登録処理
+        boolean isValid = todoService.isValid(todoData,result);
+
+        if(!result.hasErrors() && isValid){
+            Todo todo = todoData.toEntity();
+            todoRepository.saveAndFlush(todo);
+            return showTodoList(mv);
+        }else{
+            mv.setViewName("todoForm");
+            return mv;
+        }
+    }
+
+    @PostMapping("/todo/cancel")
+    public String cancel() {
+        //一覧画面に遷移
+        System.out.println("cancel");
+        return "redirect:/todo";
     }
     
 
